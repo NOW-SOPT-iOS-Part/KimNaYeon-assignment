53d65f64b244a6b2282d237db2aaa1dd81b5e6c5
Nya128 <nanalovesbap@gmail.com>
2024-04-16 01:31:25 +0900

    #1 feat: BaseView 생성

--- assignment/Presentation/Common/BaseView.swift
+++ assignment/Presentation/Common/BaseView.swift
@@ -1,5 +1,5 @@
 //
-//  BaseViewController.swift
+//  BaseView.swift
 //  assignment
 //
 //  Created by 김나연 on 4/16/24.
@@ -7,25 +7,23 @@
 
 import UIKit
 
-class BaseViewController: UIViewController {
+class BaseView: UIView {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        bindViewModel()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         setStyle()
         setLayout()
         setDelegate()
-        setRegister()
     }
     
-    /// Data와 UI를 bind 합니다.
-    func bindViewModel() {}
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     /// View의 Style을 set 합니다.
     func setStyle() {}
     /// View의 Layout을 set 합니다.
     func setLayout() {}
     /// View의 Delegate을 set 합니다.
     func setDelegate() {}
-    /// View의 Register를 set 합니다.
-    func setRegister() {}
 }
