928e087af46b08609637889aabb46e3b25fcfef8
Nya128 <nanalovesbap@gmail.com>
2024-04-16 00:24:34 +0900

    #1 config: 프로젝트 초기 세팅

---
+++ assignment/Presentation/ViewControllers/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  assignment
+//
+//  Created by 김나연 on 4/16/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
