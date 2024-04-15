0082425a172c2251432cb818c67001eb434231aa
Nya128 <nanalovesbap@gmail.com>
2024-04-16 00:02:14 +0900

    Initial Commit

---
+++ assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment
+//
+//  Created by 김나연 on 4/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
